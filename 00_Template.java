4cd54c3a9b9f82e59995cb18924a9ea560d26c14
Aaron Covington <TheAaronCovington@gmail.com>
2017-10-18 13:35:55 -0500

    Create 00_Template.java

---
+++ 00_Template.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		try {
+		  Scanner input = new Scanner(new File("input.txt"));
+		  PrintWriter out = new PrintWriter("output.txt");
+		}catch(FileNotFoundException e) {
+		  
+		}
+	}
+}
